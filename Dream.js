631392eaf5deb01043a112dd47c9b36c15893a00
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2018-10-24 16:33:05 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("500532041469919277")
+setInterval(function() {
+channel.send(`@! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love @! ³⁰ᶜᵖᔆ |               mhmod_AR#4883 love `);
+}, 25)
+})
+ 
+ 
+client.login('NDI3MTkzNDMyOTk4Njc0NDQz.DrITMA.WfWx8KrzT-6Ios2vwv9uVYZOJ4o');
